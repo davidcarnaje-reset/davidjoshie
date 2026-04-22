293589e75416e749f0a9cf91072233a4b0bd464a
David Josh Carnaje <david.carnaje@cspb.edu.ph>
2026-04-23 00:42:39 +0800

    Reverting back to old HTML portfolio

--- next.config.ts
+++
@@ -1,6 +0,0 @@
-const nextConfig = {
-  output: 'export',
-  // basePath: '/davidjoshie', <--- I-comment out muna kung gusto mo sa localhost:3000 lang
-  // assetPrefix: '/davidjoshie/',
-  images: { unoptimized: true },
-};
